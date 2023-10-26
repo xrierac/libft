194bac37348eeb11377f91bce3de3904c17e2e4e
Xavier Riera Carrion <xriera-c@student.hive.fi>
2023-10-26 11:06:37 +0300

    Remove .o files and update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 14:38:31 by xriera-c          #+#    #+#             */
-/*   Updated: 2023/10/26 10:53:36 by xriera-c         ###   ########.fr       */
+/*   Updated: 2023/10/26 11:04:32 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,9 @@ int	ft_isalpha(int c);
 int	ft_islower(int c);
 int	ft_isupper(int c);
 int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+size_t	ft_strlen(const char *s);
 
 #endif
