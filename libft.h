76ad5d4399c67434095740e858e799d0f21e6944
xrierac <xriera-c@student.hive.fi>
2023-10-30 14:15:10 +0200

    New memcmp and tolower

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 14:38:31 by xriera-c          #+#    #+#             */
-/*   Updated: 2023/10/30 10:48:30 by xriera-c         ###   ########.fr       */
+/*   Updated: 2023/10/30 13:49:53 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,11 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
+int ft_tolower(int c);
+int	ft_toupper(int c);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
