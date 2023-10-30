85ab4fa437c38256be4b2c318b6cdcff95ad680e
xrierac <xriera-c@student.hive.fi>
2023-10-30 09:41:16 +0200

    New batch of corrections

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 09:58:06 by xriera-c          #+#    #+#             */
-/*   Updated: 2023/10/24 12:56:01 by xriera-c         ###   ########.fr       */
+/*   Updated: 2023/10/30 09:36:58 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char		*dst2;
 	const unsigned char	*src2;
 
+	if (dst == '\0' && src == '\0')
+		return (dst);
 	dst2 = dst;
 	src2 = src;
 	while (n > 0)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 14:38:31 by xriera-c          #+#    #+#             */
-/*   Updated: 2023/10/26 11:04:32 by xriera-c         ###   ########.fr       */
+/*   Updated: 2023/10/30 09:23:56 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,16 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+/*void	*ft_memmove(void *dst, const void *src, size_t len);*/
+void	*ft_memset(void *b, int c, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-
+char	*ft_strtrim(char const *s1, char const *set);
 size_t	ft_strlen(const char *s);
+
 
 #endif
