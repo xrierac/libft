8adb53afdb184ff83094d1d06bcd71aac0be27f5
Xavier Riera Carrion <xriera-c@student.hive.fi>
2023-11-01 08:19:43 +0200

    Modify ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,15 +14,15 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
+	size_t	needle_len;
 
+	needle_len = ft_strlen(needle);
 	if ((!*haystack || !*needle) && len == 0)
 		return ((char *)haystack);
 	while (len > 0 && *haystack)
 	{
-		i = 0;
-		if (haystack[i] == needle[i])
-			;
+		if (ft_strncmp(haystack, needle, needle_len)
+				return ((char *)haystack);
 		len++;
 		haystack++;
 	}
