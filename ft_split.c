5d9fbf5daa149e5da17f28a5febbf8c65e263ded
Xavier Riera Carrion <xriera-c@student.hive.fi>
2023-11-02 12:33:07 +0200

    First part of ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -9,13 +9,43 @@
 /*   Updated: 2023/11/01 14:50:56 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
-#include "libft.h"
 
+#include "libft.h"
+static int	get_number_words(char const *s, char c)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	while (*s)
+	{
+		if (*s != c && j == 0)
+		{
+			i++;
+			j++;
+		}
+		if (*s == c && j != 0)
+			j = 0;
+		s++;
+	}
+	return (i);
+}
+		
 char	**ft_split(char const *s, char c)
 {
 	char	*str;
 	char	**arr;
+	size_t	end;
+	size_t	i;
 
-	
-	}*/
+	while (*s)
+	{
+		i = 0;
+		while (s[i++] != c)
+			end = i;
+		if (i > 1)
+		{
+			ft_substr(*s, 0, end);
+	}
+}
