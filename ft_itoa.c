29ff94a71b29a57e5869943beb26a1ba1f9c4ef6
Xavier Riera Carrion <xriera-c@student.hive.fi>
2023-11-03 00:04:47 +0200

    Quick nonworking version of itoa

---
+++ ft_itoa.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+char	*ft_itoa(int n)
+{
+	char		*str;
+	unsigned int	i;
+	unsigned int	sign;
+
+	str = malloc(sizeof(char) * 12);
+	if (!str)
+		return (0);
+	if (n < 0)
+	{
+		sign = - 1;
+	else
+		sign = 1;
+	i = sign * n;
+	while (i != 0)
+	{
+		
+	}
+	return (&str[0]);
+
