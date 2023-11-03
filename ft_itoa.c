27a4545645fb015b7c240df87b28ecaf3d167da5
xrierac <xriera-c@student.hive.fi>
2023-11-03 13:05:48 +0200

    Add intsize function

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,4 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 13:01:22 by xriera-c          #+#    #+#             */
+/*   Updated: 2023/11/03 13:05:05 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+
+int	sizeint(int n)
+{
+	int	i;
+	
+	if (n <= 0)
+		i = 1;
+	else
+		i = 0;
+	while (n != 0)
+	{
+		i++;
+		n = n / 10;
+	}
+	return (i);
+}
 
 char	*ft_itoa(int n)
 {
@@ -6,7 +34,7 @@ char	*ft_itoa(int n)
 	unsigned int	i;
 	unsigned int	sign;
 
-	str = malloc(sizeof(char) * 12);
+	str = malloc(sizeof(char) * sizeint(n));
 	if (!str)
 		return (0);
 	if (n < 0)
