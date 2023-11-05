ab01593806c2b82021ef50f7950559600949cce7
Xavier Riera Carrion <xriera-c@student.hive.fi>
2023-11-05 16:25:40 +0200

    Small correction but lacks one more

--- ft_atoi.c
+++ ft_atoi.c
@@ -9,6 +9,7 @@
 /*   Updated: 2023/10/31 15:44:01 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_atoi(const char *nptr)
@@ -16,6 +17,8 @@ int	ft_atoi(const char *nptr)
 	int	i;
 	int	sign;
 
+	if (!nptr)
+		return (0);
 	i = 0;
 	sign = 1;
 	while (ft_isspace(*nptr))
