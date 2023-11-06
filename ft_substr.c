4cbe007fb962181b7be9988d39b0da021ffcc32a
xrierac <xriera-c@student.hive.fi>
2023-11-06 12:04:55 +0200

    Small correction for substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 09:04:32 by xriera-c          #+#    #+#             */
-/*   Updated: 2023/11/06 10:32:32 by xriera-c         ###   ########.fr       */
+/*   Updated: 2023/11/06 12:03:47 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s || start >= ft_strlen(s) || len == 0)
 		return (ft_strdup(""));
-	if (len > ft_strlen(s))
+	if ((len + start) > ft_strlen(s))
 		len = ft_strlen(s) - start;
 	substring = malloc(sizeof(char) * (len + 1));
 	if (!substring)
