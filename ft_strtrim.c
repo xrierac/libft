aa1ac89126e79bc4eecf192de6e0c53a3190418b
Xavier Riera Carrion <xriera-c@student.hive.fi>
2023-12-06 10:07:08 +0200

    Remove in_set function

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,16 +12,6 @@
 
 #include "libft.h"
 
-static int	in_set(char c, char const *set)
-{
-	if (c == '\0')
-		return (0);
-	while (*set)
-		if (c == *set++)
-			return (1);
-	return (0);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
@@ -32,11 +22,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (ft_strdup(""));
 	start = 0;
 	end = ft_strlen(s1) - 1;
-	while (in_set(s1[start], set))
+	while (ft_inset(s1[start], set))
 		start++;
 	if (start > end)
 		return (ft_strdup(""));
-	while (in_set(s1[end], set))
+	while (ft_inset(s1[end], set))
 		end--;
 	str = ft_substr(s1, start, end - start + 1);
 	return (str);
